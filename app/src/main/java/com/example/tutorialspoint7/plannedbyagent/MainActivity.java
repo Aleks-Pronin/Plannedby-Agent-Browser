48d1c1ca72be5d0ff707b58f2af63f95bab99848
Aleks-Pronin <sovet207@gmail.com>
2023-06-08 10:45:20 +0300

    First build of work browser.

--- app/src/main/java/com/example/tutorialspoint7/plannedbyagent/MainActivity.java
+++ app/src/main/java/com/example/tutorialspoint7/plannedbyagent/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tutorialspoint7.plannedbyagent;
 
 import android.annotation.TargetApi;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences.Editor editor = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE).edit();
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(new MyWebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://plannedby.hz.cz/");
+        SharedPreferences prefs = getSharedPreferences("MY_PREFS_NAME", MODE_PRIVATE);
+        if(prefs.getInt("registered", 0)>0){
+        webView.loadUrl("https://plannedby.hz.cz/?reg");
+        editor.putInt("scoreViewA", 1);}
+        else{webView.loadUrl("https://plannedby.hz.cz/");}
     }
 
     private WebView webView;
